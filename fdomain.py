bc59cb86257d340d4c14e2894f74ccd1532bae3e
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2023-02-16 23:58:03 +0530

    Create fdomain.py

---
+++ fdomain.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import re,sys,random
+
+class DomainFilter:
+    def __init__(self, inputfile):
+        self.inputfile = inputfile
+
+    def read_text(self):
+        with open(self.inputfile, "r") as file:
+            self.text = file.read()
+
+    def find_domains(self):
+        self.domains = re.findall(r"(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}", self.text)
+
+    def write_filtered_domains(self):
+        random_int = random.randint(1, 100000)
+        filename = f"{self.inputfile}_Filtered_Domains_{random_int}"
+        with open(filename, "w") as file:
+            file.write("\n".join(self.domains))
+
+if __name__ == "__main__":
+    inputfile = sys.argv[1]
+    domain_filter = DomainFilter(inputfile)
+    domain_filter.read_text()
+    domain_filter.find_domains()
+    domain_filter.write_filtered_domains()
