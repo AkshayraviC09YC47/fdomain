c1461fe89cd3be1689552ef4e8f1d217f7b927c7
Akshay Ravi <58636452+AkshayraviC09YC47@users.noreply.github.com>
2023-03-05 11:42:14 +0530

    Update fdomain.py

--- fdomain.py
+++ fdomain.py
@@ -1,32 +1,39 @@
 #!/usr/bin/env python3
 
-import re,sys, random
+import re, sys, random
 
 class DomainFilter:
-    def __init__(self, inputfile):
+    def __init__(self, inputfile=None):
         self.inputfile = inputfile
 
     def read_text(self):
-        with open(self.inputfile, "r") as file:
-            self.text = file.read()
+        if self.inputfile:
+            with open(self.inputfile, "r") as file:
+                self.text = file.read()
+        else:
+            if not sys.stdin.isatty():
+                self.text = sys.stdin.read()
+            else:
+                print("---> Error: No input file or pipe provided.")
+                print("---> Usage: 'cat urls.txt|beta-fdomain' or 'python3 fdomain.py <input_file>'")
+                sys.exit(1)
 
     def find_domains(self):
         self.domains = re.findall(r"(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-]{,61}[a-zA-Z0-9])?\.)+[a-zA-Z]{2,6}", self.text)
 
     def write_filtered_domains(self):
         random_int = random.randint(1, 100000)
-        filename = f"{self.inputfile}_Filtered_Domains_{random_int}"
+        filename = f"{self.inputfile or 'Pipe_'}Filtered_Domains_{random_int}"
         with open(filename, "w") as file:
             file.write("\n".join(self.domains))
+            print(f"---> Result Saved as {filename}")
 
 if __name__ == "__main__":
-    if len(sys.argv) < 1:
-        print("Error: No input file provided.")
-        print("Usage: fdomain <input_file>")
-        print("Usage: python3 fdomain.py <input_file>")
-        sys.exit(1)
-    inputfile = sys.argv[1]
-    domain_filter = DomainFilter(inputfile)
+    if len(sys.argv) < 2:
+        domain_filter = DomainFilter()
+    else:
+        inputfile = sys.argv[1]
+        domain_filter = DomainFilter(inputfile)
     domain_filter.read_text()
     domain_filter.find_domains()
     domain_filter.write_filtered_domains()
